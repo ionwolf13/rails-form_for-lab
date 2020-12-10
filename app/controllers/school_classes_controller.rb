7ca1b4ef0668bea2c4e00ed3bf59b4cf78c7f393
ionwolf13 <voli.scorp13@gmail.com>
2020-12-10 17:12:21 -0600

    commit tree 6fb0a94f24bab2e5a4e39e43c96a1dd2bd92666d

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,43 @@
+class SchoolClassesController < ApplicationController
+
+    def new
+        @schoolclass = SchoolClass.new
+    end
+
+    def create
+        @schoolclass = SchoolClass.create(school_class_params)
+        redirect_to school_class_path(@schoolclass)
+    end
+
+    def show
+        @schoolclass = SchoolClass.find(params[:id])
+    end
+
+    def index
+        @school_classes = SchoolClass.all
+    end
+
+    def edit
+        @schoolclass = SchoolClass.find(params[:id])
+    end
+
+    def update
+        @schoolclass = SchoolClass.find(params[:id])
+        byebug
+        @schoolclass.update(title: params[:school_class][:title], room_number: params[:school_class][:room_number])
+        redirect_to school_class_path(@schoolclass)
+    end
+
+
+    private
+
+    def school_class_params
+        params.require(:school_class).permit(:title , :room_number)
+    end
+
+
+
+
+
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.destroy_all
+SchoolClass.destroy_all
+
+stu1 = Student.create(first_name: "Jesus", last_name: "Gar")
+stu2 = Student.create(first_name: "Israel", last_name:"Gar")
+stu3 = Student.create(first_name: "Garcia", last_name:"Gar")
+stu4 = Student.create(first_name: "Ira", last_name:"Gar")
+stu5 = Student.create(first_name: "Zeus", last_name:"Gar")
+
+
+sc1 = SchoolClass.create(title: "science", room_number: 1)
+sc2 = SchoolClass.create(title: "math", room_number: 2)
+sc3 = SchoolClass.create(title: "history", room_number: 3)
+sc4 = SchoolClass.create(title: "biology", room_number: 4)
